708fb52ba196c42bc1b0157914f487cd21b2381f
Yasminta24-meet <109975287+YasminQT@users.noreply.github.com>
2023-07-17 18:09:28 +0300

    lab+bonus is done

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function changeBackgroundColor(color) {
-  document.body.style.backgroundColor = color;
+function changeBackgroundColor(color){
+	document.body.style.backgroundColor = color;
+}
+function toggle() {
+  if (document.body.style.backgroundColor == 'brown' ) {
+  	changeBackgroundColor("darkgray");
+  	console.log('hi!');
+  }
+  else {
+  	changeBackgroundColor("brown");
+  }
 }
